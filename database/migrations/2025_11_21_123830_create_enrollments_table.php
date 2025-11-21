b8ea1f21b5182559f375deb919a3724579f453a8
pchan132 <poepor132@gmail.com>
2025-11-21 21:14:58 +0700

    migration-coures-enrollments

---
+++ database/migrations/2025_11_21_123830_create_enrollments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+
+            // ไม่ให้นักเรียนคนเดียวกันลงทะเบียนซ้ำในคอร์สเดียวกัน
+            $table->unique(['course_id', 'student_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
