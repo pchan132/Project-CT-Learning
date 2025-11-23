438909d5b5b57e2320f2aa211ac537860071880c
pchan132 <poepor132@gmail.com>
2025-11-23 09:22:03 +0700

    migrate

---
+++ database/migrations/2025_11_23_021029_create_lessons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('module_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->enum('content_type', ['PDF','VIDEO','TEXT']);
+            $table->string('content_url')->nullable(); // for file path or video/url
+            $table->longText('content_text')->nullable(); // for TEXT content
+            $table->unsignedInteger('order')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
